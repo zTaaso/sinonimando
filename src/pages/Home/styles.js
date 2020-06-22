fa42232bd5edce9301f9e2a95372d7665463ea10
ztaaso <ztaaso.cedaspy@gmail.com>
2020-06-22 10:32:15 -0300

    home styles added

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100%;
+  min-height: 100%;
   background: radial-gradient(#5c2a9d, #45046a);
   display: flex;
   align-items: center;
@@ -11,9 +11,9 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  min-height: 100px;
+  min-height: 100%;
   width: 90%;
-  padding: 20px 30px;
+  padding: 20px 30px 30px;
   border-radius: 8px;
   background: #b5076b;
   margin-top: 50px;
@@ -21,6 +21,12 @@ export const Content = styled.div`
 
 export const SynonymItem = styled.li`
   list-style: none;
+  padding-bottom: 30px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.195);
+
+  &:last-child {
+    border-bottom: none;
+  }
 
   & + & {
     margin-top: 15px;
@@ -28,14 +34,13 @@ export const SynonymItem = styled.li`
 
   h2 {
     color: #fff;
-    margin-bottom: 6px;
     font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
       'Helvetica Neue', sans-serif;
   }
 
   div {
     margin-left: 15px;
-    padding: 5px 20px;
+    padding: 0 20px;
     display: inline-block;
     max-width: 100%;
     word-break: break-word;
@@ -54,14 +59,31 @@ export const SynonymItem = styled.li`
       list-style: none;
       /* min-width: 80px; */
 
-      a {
-        color: #fff;
-        font-size: 18px;
+      button {
+        background: none;
+        border: none;
+        color: rgba(255, 255, 255, 0.85);
+        font-size: 20px;
         text-decoration: none;
-        border-bottom: 1px dotted #ff0;
-        line-height: 1.5;
+        border-bottom: 1px dotted rgba(255, 255, 255, 0.85);
+
+        line-height: 1.7;
 
         cursor: pointer;
+
+        &:hover {
+          color: #3aa5dc;
+          border-bottom: 1px solid rgba(255, 255, 255, 0.85);
+        }
+
+        &.multiple {
+          border-bottom: none;
+          cursor: not-allowed;
+        }
+
+        &.multiple:hover {
+          color: rgba(255, 255, 255, 0.85);
+        }
       }
     }
   }
