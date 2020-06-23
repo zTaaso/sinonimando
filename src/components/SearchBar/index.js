24b5fae3946c2ff508a1e5f41ef604b35a92e738
ztaaso <ztaaso.cedaspy@gmail.com>
2020-06-23 10:34:34 -0300

    seachBar component created

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdSearch } from 'react-icons/md';
 
 import { StatusBar } from './styles';
 
-function SearchBar() {
+function SearchBar({ onSearch, onWordChange, wordProp }) {
+  const [word, setWord] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSearch(word);
+  }
+
+  useEffect(() => {
+    console.log(wordProp);
+    setWord(wordProp);
+  }, [wordProp]);
+
   return (
     <StatusBar>
       <h1>Sinonimando</h1>
 
-      <form action="">
-        <input type="text" placeholder="Procure por um sinônimo" />
-        <button type="button">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(evt) => {
+            onWordChange(evt.target.value);
+            setWord(evt.target.value);
+          }}
+          value={word}
+          placeholder="Procure por um sinônimo"
+        />
+        <button type="submit">
           <MdSearch size={20} color="#FFF" />
         </button>
       </form>
