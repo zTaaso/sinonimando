dc4643bdb4c7ae3d7a6dbb9eeb5d8721242da0e9
ztaaso <ztaaso.cedaspy@gmail.com>
2020-06-24 18:03:53 -0300

    styled components created

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,13 +7,62 @@ export const Container = styled.div`
   align-items: center;
   flex: 1;
   flex-direction: column;
+  padding: 5px 0 50px;
 `;
 
 export const Content = styled.div`
   min-height: 100px;
   width: 90%;
-  padding: 40px 90px;
+  padding: 20px 30px;
   border-radius: 8px;
   background: #b5076b;
   margin-top: 50px;
+`;
+
+export const SynonymItem = styled.li`
+  list-style: none;
+
+  & + & {
+    margin-top: 15px;
+  }
+
+  h2 {
+    color: #fff;
+    margin-bottom: 6px;
+    font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+      'Helvetica Neue', sans-serif;
+  }
+
+  div {
+    margin-left: 15px;
+    padding: 5px 20px;
+    display: inline-block;
+    max-width: 100%;
+    word-break: break-word;
+    overflow-wrap: break-word;
+
+    ul {
+      display: flex;
+      flex-direction: row;
+      max-width: 100%;
+      overflow-wrap: break-word;
+      flex-wrap: wrap;
+    }
+
+    ul li {
+      margin-right: 6px;
+      list-style: none;
+      /* min-width: 80px; */
+
+      a {
+        color: #fff;
+        font-size: 18px;
+        text-decoration: none;
+        border-bottom: 1px dotted #ff0;
+        line-height: 1.5;
+
+        cursor: pointer;
+      }
+    }
+  }
 `;
