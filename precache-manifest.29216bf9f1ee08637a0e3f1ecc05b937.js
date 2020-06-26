be6e4b5664c24151f58d7f22d7ad087a8881c539
ztaaso <ztaaso.cedaspy@gmail.com>
2020-06-26 12:16:29 -0300

    Updates

---
+++ precache-manifest.29216bf9f1ee08637a0e3f1ecc05b937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db385c7c5a0e54dcd7a30fa66e10afe",
+    "url": "/sinonimando/index.html"
+  },
+  {
+    "revision": "fa5d1b6678ec283580c1",
+    "url": "/sinonimando/static/css/main.b6288138.chunk.css"
+  },
+  {
+    "revision": "01902dc7ef1f436dd21b",
+    "url": "/sinonimando/static/js/2.97c500b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sinonimando/static/js/2.97c500b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5d1b6678ec283580c1",
+    "url": "/sinonimando/static/js/main.c09dbfde.chunk.js"
+  },
+  {
+    "revision": "3c86950dd2d05486855d",
+    "url": "/sinonimando/static/js/runtime-main.d30269fe.js"
+  }
+]);
