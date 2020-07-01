93c9ff6d27607341b8121057bc56ef0521bb05f1
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-01 18:10:12 -0300

    testing url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    'https://cors-anywhere.herokuapp.com/https://sinonimosapi.netlify.com/.netlify/functions/api/',
+  baseURL: 'http://sinonimosapi.netlify.com/.netlify/functions/api/',
   method: 'GET',
 });
 
