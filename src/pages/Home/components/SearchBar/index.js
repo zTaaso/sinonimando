35dd042a518d3bf1bd881a97556f21d7abec294a
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-08 00:20:04 -0300

    components moved

---
+++ src/pages/Home/components/SearchBar/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { MdSearch } from 'react-icons/md';
+
+import { StatusBar } from './styles';
+
+function SearchBar({ routeWord, onWordChange, wordProp = '' }) {
+  const history = useHistory();
+  const [word, setWord] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (routeWord === word) return;
+
+    history.push(`/${word}`);
+  }
+
+  useEffect(() => {
+    setWord(wordProp);
+  }, [wordProp]);
+
+  return (
+    <StatusBar>
+      <h1>Sinonimando</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(evt) => {
+            onWordChange(evt.target.value);
+            setWord(evt.target.value);
+          }}
+          value={word}
+          placeholder="Procure por um sinônimo"
+        />
+        <button type="submit">
+          <MdSearch size={20} color="#FFF" />
+        </button>
+      </form>
+    </StatusBar>
+  );
+}
+
+export default SearchBar;
