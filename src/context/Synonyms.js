d308317fba894707fe6fb3f9e5b3ba14171b354b
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-11 12:36:01 -0300

    context created

--- src/context/Synonyms.js
+++ src/context/Synonyms.js
@@ -1,7 +1,61 @@
-import React, { useContext, createContext } from 'react';
+import React, { useState, useCallback, createContext, useEffect } from 'react';
 
-const SynonymsContext = createContext();
+import api from '../services/api.js';
+import { useHistory } from 'react-router-dom';
+
+export const SynonymsContext = createContext();
 
 export default function SynonymsProvider({ children }) {
-  return <SynonymsContext.Provider value={}></SynonymsContext.Provider>;
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(false);
+  const [synonyms, setSynonyms] = useState([]);
+  const [word, setWord] = useState('');
+
+  const getSynonyms = useCallback(async (word = '') => {
+    setLoading(true);
+    const { data } = await api({ params: { q: word.trim() } });
+
+    const synonyms = Object.entries(data).map((synonym) => ({
+      sort: synonym[0],
+      synonyms: synonym[1].map((word, index, arr) => ({
+        name: index === arr.length - 1 ? word.concat('.') : word.concat(','),
+        isMultiple: word.includes(' '),
+      })),
+    }));
+
+    setSynonyms(synonyms);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (word === 'notFound' || !word) return;
+
+    getSynonyms(word);
+    setWord(word);
+  }, [getSynonyms, word, setWord]);
+
+  useEffect(() => {
+    if (word === 'notFound' || !word) return;
+
+    if (!synonyms[0]) {
+      history.push('/notFound');
+    }
+
+    /* eslint-disable */
+  }, [synonyms]);
+  /* eslint-enable */
+
+  const state = {
+    getSynonyms,
+    synonyms,
+    loading,
+    wordState: { word, setWord },
+  };
+
+  return (
+    <SynonymsContext.Provider value={state}>
+      {children}
+    </SynonymsContext.Provider>
+  );
 }
