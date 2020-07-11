83ec0e9ef0809b33ff2514af2f3c7ecae3088736
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-11 12:38:11 -0300

    home refactored

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,71 +1,16 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
 import { Container } from './styles.js';
 
 import SearchBar from './components/SearchBar';
 
-import NotFound from './components/NotFound';
-import SynonymsContent from './components/SynonymsContent';
-import Welcome from './components/Welcome';
+import Router from './router';
 
-import api from '../../services/api';
-
-function Home({ match, notFound = false, isHome }) {
-  const history = useHistory();
-  const routeWord = match.params.word;
-  const { url } = match;
-
-  const [synonyms, setSynonyms] = useState([]);
-  const [word, setWord] = useState(routeWord);
-  const [loading, setLoading] = useState(false);
-  // const [isHome, setIsHome] = useState(url === '/');
-
-  const getSynonyms = useCallback(async (word = '') => {
-    setLoading(true);
-    const { data } = await api({ params: { q: word.trim() } });
-
-    const synonyms = Object.entries(data).map((synonym) => ({
-      sort: synonym[0],
-      synonyms: synonym[1].map((word, index, arr) => ({
-        name: index === arr.length - 1 ? word.concat('.') : word.concat(','),
-        isMultiple: word.includes(' '),
-      })),
-    }));
-
-    setSynonyms(synonyms);
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    if (!synonyms[0]) {
-      history.push('/notFound');
-    } else {
-      // setIsHome(false);
-    }
-  }, [history, synonyms]);
-
-  useEffect(() => {
-    getSynonyms(routeWord);
-    setWord(routeWord);
-  }, [routeWord, getSynonyms]);
-
+function Home() {
   return (
     <Container>
-      <SearchBar
-        onSearch={getSynonyms}
-        onWordChange={() => {}}
-        wordProp={word}
-        routeWord={routeWord}
-      />
-
-      {isHome ? (
-        <Welcome />
-      ) : notFound ? (
-        <NotFound />
-      ) : (
-        <SynonymsContent synonyms={synonyms} word={word} loading={loading} />
-      )}
+      <SearchBar />
+      <Router />
     </Container>
   );
 }
